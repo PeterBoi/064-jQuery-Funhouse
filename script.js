324fb87defa485d239ca8b1e4d568c49e4a134af
Pedro Cruz Avalos <106279254+PeterBoi@users.noreply.github.com>
2022-06-01 20:39:43 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeIn();
+    $(".pikachu").slideToggle();
+});
+
+$(".light-switch").click(function() {
+    $(".light-bulb").toggle();
+});
+
+$(".remote").click(function() {
+    $(".flat-screen").fadeToggle();
+});
